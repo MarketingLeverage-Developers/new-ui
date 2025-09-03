72201bca4832652df79c6e3cc2cff9b69336d861
cheolwooklee <lcw5745187@mleverage.co.kr>
2025-09-03 14:09:18 +0900

    fix : RoundedTab select, div props 분리

--- RoundedTab/RoundedTab.tsx
+++ RoundedTab/RoundedTab.tsx
@@ -3,15 +3,21 @@ import styles from './RoundedTab.module.scss';
 import Select from '@/shared/headless/Select/Select';
 import { Item } from './components/Item';
 
-type RoundedTabProps = React.ComponentProps<typeof Select> & React.HTMLAttributes<HTMLDivElement>;
+type RoundedTabProps = React.ComponentProps<typeof Select> & {
+    divProps?: Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'>;
+};
 
-const RoundedTab = ({ children, style, ...props }: RoundedTabProps) => (
-    <Select {...props}>
-        <div {...props} className={styles.RoundedTab} style={{ ...style }}>
-            {children}
-        </div>
-    </Select>
-);
+const RoundedTab = ({ children, divProps, ...props }: RoundedTabProps) => {
+    const { style, ...restDiv } = divProps ?? {};
+
+    return (
+        <Select {...props}>
+            <div {...restDiv} className={styles.RoundedTab} style={{ ...style }}>
+                {children}
+            </div>
+        </Select>
+    );
+};
 
 export default RoundedTab;
 
