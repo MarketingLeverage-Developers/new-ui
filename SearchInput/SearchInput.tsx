f69508734cffde81377c980c2791a5ed5d848e08
cheolwooklee <lcw5745187@mleverage.co.kr>
2025-09-05 16:13:05 +0900

    feat: 공통 컴포넌트 SearchInput

---
+++ SearchInput/SearchInput.tsx
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react';
+import styles from './SearchInput.module.scss';
+import { MdOutlineCancel } from 'react-icons/md';
+import { CiSearch } from 'react-icons/ci';
+
+type SearchInputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+const SearchInput = ({ value, onChange, ...props }: SearchInputProps) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const clearHandler = () => {
+        if (onChange) {
+            onChange({ target: { value: '' } } as React.ChangeEvent<HTMLInputElement>);
+        } else if (inputRef.current) {
+            inputRef.current.value = '';
+        }
+    };
+
+    return (
+        <div className={styles.SearchInputWrapper}>
+            <input ref={inputRef} value={value} onChange={onChange} {...props} />
+            <div className={styles.SearchIcons}>
+                <CiSearch />
+            </div>
+            {String(value ?? inputRef.current?.value ?? '').length > 0 && (
+                <div className={styles.CancelIcons}>
+                    <MdOutlineCancel style={{ cursor: 'pointer' }} onClick={clearHandler} />
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default SearchInput;
