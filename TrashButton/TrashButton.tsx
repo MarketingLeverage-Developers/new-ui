2d7f012dd5035c4f8ca57ca09fb32e551cd6b0ad
cheolwooklee <lcw5745187@mleverage.co.kr>
2025-09-10 11:55:59 +0900

    fix : TrashButton OnClick 수정

--- TrashButton/TrashButton.tsx
+++ TrashButton/TrashButton.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { BiSolidTrashAlt } from 'react-icons/bi';
 import styles from './TrashButton.module.scss';
 
-type TrashButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-    onClick?: () => void;
-};
+type TrashButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const TrashButton = ({ onClick, ...props }: TrashButtonProps) => (
     <button {...props} className={styles.TrashButton} onClick={onClick}>
