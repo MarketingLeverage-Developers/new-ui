3d7e6f59d9af3d44b884575baeafcb631b8bbcfc
cheolwooklee <lcw5745187@mleverage.co.kr>
2025-09-10 11:19:19 +0900

    feat: SectionDiver 추가

---
+++ SectionDiver/SectionDiver.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './SectionDiver.module.scss';
+
+const SectionDiver = () => <div className={styles.SectionDiver}></div>;
+
+export default SectionDiver;
