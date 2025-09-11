e07992d752e21497bd31c8b2727f9991d207d39c
mleverage-Intern-kwonkibeom <krshlee22@mleverage.co.kr>
2025-09-11 14:08:33 +0900

    feat(HoverButton) hover버튼 추가

---
+++ HoverButton/HoverButton.tsx
@@ -0,0 +1,34 @@
+import Text from '@/shared/primitives/Text/Text';
+import styles from './HoverButton.module.scss';
+import type { CSSVariables } from '@/shared/types/css/CSSVariables';
+import { toCssUnit } from '@/shared/utils';
+import { toCssPadding } from '@/shared/utils/css/toCssPadding';
+import type { PaddingSize } from '@/shared/types/css/PaddingSize';
+import type { HexColor } from '@/shared/types/css/HexColor';
+import type { ThemeColorVar } from '@/shared/types/css/ThemeColorTokens';
+
+type HoverButtonProps = {
+    onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+    width?: string | number;
+    padding?: PaddingSize | number;
+    textColor?: HexColor | ThemeColorVar;
+    style: React.CSSProperties;
+    children: React.ReactNode;
+};
+
+const HoverButton = ({ onClick, width, padding, textColor, children, style, ...props }: HoverButtonProps) => {
+    const cssVariables: CSSVariables = {
+        '--width': toCssUnit(width),
+        '--padding': toCssPadding(padding),
+        '--textColor': textColor,
+    };
+    return (
+        <button className={styles.HoverButton} onClick={onClick} style={{ ...style, ...cssVariables }} {...props}>
+            <Text fontSize={13} fontWeight={500}>
+                {children}
+            </Text>
+        </button>
+    );
+};
+
+export default HoverButton;
