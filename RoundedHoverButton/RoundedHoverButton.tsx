f1d05e56e98334d714cf71e06402ed78cfa403a1
mleverage-Intern-kwonkibeom <krshlee22@mleverage.co.kr>
2025-09-11 14:15:39 +0900

    feat(RoundedHoverButton) HoverButton 명칭 RoundedHoverButton으로 수정

--- RoundedHoverButton/RoundedHoverButton.tsx
+++ RoundedHoverButton/RoundedHoverButton.tsx
@@ -1,10 +1,10 @@
-import styles from './HoverButton.module.scss';
+import styles from './RoundedHoverButton.module.scss';
 import type { CSSVariables } from '@/shared/types/css/CSSVariables';
 import { toCssUnit } from '@/shared/utils';
 import { toCssPadding } from '@/shared/utils/css/toCssPadding';
 import type { PaddingSize } from '@/shared/types/css/PaddingSize';
 
-type HoverButtonProps = {
+type RoundedHoverButtonProps = {
     onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
     width?: string | number;
     padding?: PaddingSize | number;
@@ -12,16 +12,21 @@ type HoverButtonProps = {
     children: React.ReactNode;
 };
 
-const HoverButton = ({ onClick, width, padding, children, style, ...props }: HoverButtonProps) => {
+const RoundedHoverButton = ({ onClick, width, padding, children, style, ...props }: RoundedHoverButtonProps) => {
     const cssVariables: CSSVariables = {
         '--width': toCssUnit(width),
         '--padding': toCssPadding(padding),
     };
     return (
-        <button className={styles.HoverButton} onClick={onClick} style={{ ...style, ...cssVariables }} {...props}>
+        <button
+            className={styles.RoundedHoverButton}
+            onClick={onClick}
+            style={{ ...style, ...cssVariables }}
+            {...props}
+        >
             <span>{children}</span>
         </button>
     );
 };
 
-export default HoverButton;
+export default RoundedHoverButton;
