43a70dd2cd748c4c002b7e8a9741c4fa8df81d02
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-09-16 09:11:35 +0900

    feat: Desktop.Left 수정 - children 속성 옵셔널 수정

--- Desktop/components/Header/components/FirstLine/components/Left/Left.tsx
+++ Desktop/components/Header/components/FirstLine/components/Left/Left.tsx
@@ -2,7 +2,7 @@ import React, { type ReactNode } from 'react';
 import styles from './Left.module.scss';
 
 type LeftProps = {
-    children: ReactNode;
+    children?: ReactNode;
 };
 
 export const Left = ({ children }: LeftProps) => <div className={styles.Left}>{children}</div>;
