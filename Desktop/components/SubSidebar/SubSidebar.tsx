68738d92830c3773a558a4824a8b61235a1824f2
cheolwooklee <lcw5745187@mleverage.co.kr>
2025-09-19 13:10:06 +0900

    fix: subSidebar scrollbar scss

--- Desktop/components/SubSidebar/SubSidebar.tsx
+++ Desktop/components/SubSidebar/SubSidebar.tsx
@@ -5,8 +5,4 @@ type SubSidebarProps = {
     children: React.ReactNode;
 };
 
-export const SubSidebar = ({ children }: SubSidebarProps) => (
-    <aside className={styles.SubSidebar}>
-        <div className={styles.SubSidebarWrapper}>{children}</div>
-    </aside>
-);
+export const SubSidebar = ({ children }: SubSidebarProps) => <aside className={styles.SubSidebar}>{children}</aside>;
