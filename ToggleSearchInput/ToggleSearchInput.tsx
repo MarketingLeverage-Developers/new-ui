b7052b0e0f40a88c4a7b12c13e1a4574d976d6cf
cheolwooklee <lcw5745187@mleverage.co.kr>
2025-09-29 14:20:45 +0900

    fix : toggleSearch 수정

--- ToggleSearchInput/ToggleSearchInput.tsx
+++ ToggleSearchInput/ToggleSearchInput.tsx
@@ -3,20 +3,21 @@ import styles from './ToggleSearchInput.module.scss';
 import { CiSearch } from 'react-icons/ci';
 import Flex from '../Flex/Flex';
 import classNames from 'classnames';
-import { IoIosArrowBack } from 'react-icons/io';
+import { IoIosArrowForward } from 'react-icons/io';
 import { getThemeColor } from '@/shared/utils/css/getThemeColor';
 import RoundedSearch from '../RoundedSearch/RoundedSearch';
+import BaseButton from '../BaseButton/BaseButton';
 
 type ToggleSearchInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
     children: React.ReactNode;
+    onSearchHandler: () => void;
 };
 
-const ToggleSearchInput = ({ children, value, onChange, ...props }: ToggleSearchInputProps) => {
+const ToggleSearchInput = ({ children, value, onChange, onSearchHandler, ...props }: ToggleSearchInputProps) => {
     const [open, setOpen] = useState(false);
 
     const searchClassName = classNames(styles.SearchInput, {
-        [styles.Open]: open,
-        [styles.Off]: !open,
+        [styles.Active]: open,
     });
 
     return (
@@ -24,14 +25,27 @@ const ToggleSearchInput = ({ children, value, onChange, ...props }: ToggleSearch
             <Flex justify="space-between">
                 {children}
                 <Flex padding={5} onClick={() => setOpen(true)} className={styles.Cursor}>
-                    <CiSearch strokeWidth={1.5} size={20} color={getThemeColor('Gray1')} />
+                    {!open && <CiSearch strokeWidth={1.5} size={20} color={getThemeColor('Gray1')} />}
                 </Flex>
             </Flex>
             <div className={searchClassName}>
                 <Flex padding={5} onClick={() => setOpen(false)} className={styles.Cursor}>
-                    <IoIosArrowBack size={20} color={getThemeColor('Gray1')} />
+                    <IoIosArrowForward size={20} color={getThemeColor('Gray1')} />
                 </Flex>
                 <RoundedSearch placeholder="검색" value={value} onChange={onChange} width={'100%'} {...props} />
+
+                <BaseButton
+                    height={34}
+                    width={60}
+                    padding={{ x: 12 }}
+                    radius={6}
+                    fontSize={13}
+                    bgColor={getThemeColor('Primary1')}
+                    textColor={getThemeColor('White1')}
+                    onClick={onSearchHandler}
+                >
+                    검색
+                </BaseButton>
             </div>
         </div>
     );
