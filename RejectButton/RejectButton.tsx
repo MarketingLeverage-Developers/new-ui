773fd3aa93cdd5c0e71fa07fdd43e762f06e3abc
cheolwooklee <lcw5745187@mleverage.co.kr>
2025-11-06 16:35:04 +0900

    feat : RejectButton

---
+++ RejectButton/RejectButton.tsx
@@ -0,0 +1,59 @@
+import React, { type ButtonHTMLAttributes } from 'react';
+import styles from './RejectButton.module.scss';
+import type { HexColor } from '@/shared/types/css/HexColor';
+import type { ThemeColorVar } from '@/shared/types/css/ThemeColorTokens';
+import type { CSSVariables } from '@/shared/types/css/CSSVariables';
+import classNames from 'classnames';
+import { toCssUnit } from '@/shared/utils';
+import type { CSSLength } from '@/shared/types';
+import { Image } from '../Image/Image';
+import Flex from '../Flex/Flex';
+import removeIcon from '@/shared/assets/images/removebg-preview.svg';
+import Text from '../Text/Text';
+import type { PaddingSize } from '@/shared/types/css/PaddingSize';
+import { toCssPadding } from '@/shared/utils/css/toCssPadding';
+
+type RejectButtonPros = {
+    bgColor?: HexColor | ThemeColorVar;
+    width?: CSSLength;
+    height?: CSSLength;
+    fontSize?: CSSLength;
+    padding?: PaddingSize;
+    textColor?: HexColor | ThemeColorVar;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+const RejectButton = ({
+    bgColor = 'var(--White1)',
+    width = 94,
+    height = 48,
+    fontSize = 15,
+    textColor,
+    padding = { x: 20, y: 12 },
+    ...props
+}: RejectButtonPros) => {
+    const cssVariables: CSSVariables = {
+        '--font-size': toCssUnit(fontSize),
+        '--padding': toCssPadding(padding),
+        '--width': toCssUnit(width),
+        '--height': toCssUnit(height),
+        '--color': textColor,
+        '--background-color': bgColor,
+    };
+
+    const buttonClassName = classNames(styles.RejectButton, {
+        [styles.Disabled]: props.disabled,
+    });
+
+    return (
+        <button className={buttonClassName} style={{ ...cssVariables, ...props.style }} {...props}>
+            <Flex gap={4} align="center" justify="center">
+                <Flex padding={5}>
+                    <Image src={removeIcon} alt="RejectButton" />
+                </Flex>
+                <Text fontWeight={600}>반려</Text>
+            </Flex>
+        </button>
+    );
+};
+
+export default RejectButton;
