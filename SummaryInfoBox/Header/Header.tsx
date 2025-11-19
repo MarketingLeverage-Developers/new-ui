d135fc94e5245833f62a1ac2ceb39a73365085ea
mlyykkang <yykkang@mleverage.co.kr>
2025-11-19 15:16:01 +0900

    fix: FileBox 및 SummaryInfoBox 수정

--- SummaryInfoBox/Header/Header.tsx
+++ SummaryInfoBox/Header/Header.tsx
@@ -8,17 +8,18 @@ moment.locale('ko');
 type Props = {
     icon: string;
     requestTime?: string;
+    title?: string;
 };
-const Header = ({ icon, requestTime }: Props) => {
+const Header = ({ icon, requestTime, title = '요청 정보' }: Props) => {
     const date = moment(requestTime)?.format('YYYY.MM.DD (ddd)');
     const time = moment(requestTime)?.format('HH:mm');
     return (
         <div className={styles.SummartInfoBoxHeader}>
             <div className={styles.TitleWrapper}>
                 <img src={icon} alt="" />
-                <span>요청 정보</span>
+                <span>{title}</span>
             </div>
-            {time && (
+            {requestTime && (
                 <div className={styles.TimeWrpper}>
                     요청시간 : <span>{date}</span> <strong>{time}</strong>
                 </div>
