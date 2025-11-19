653a7c326e96fe80eed3dc3f19e7429a6b5de8cd
mlyykkang <yykkang@mleverage.co.kr>
2025-11-19 11:16:05 +0900

    feat: Request의 FileBox 생성

---
+++ FileBox/FileBox.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './FileBox.module.scss';
+import type { CSSVariables } from '@/shared/types/css/CSSVariables';
+import { toCssUnit } from '@/shared/utils';
+import type { CSSLength } from '@/shared/types';
+import { IoMdCloseCircle } from 'react-icons/io';
+
+type Props = {
+    file: any;
+    width?: CSSLength;
+    onDeleteClick?: () => void;
+};
+const FileBox = ({ file, width, onDeleteClick }: Props) => {
+    const cssVariables: CSSVariables = {
+        '--width': toCssUnit(width),
+    };
+    return (
+        <>
+            {file && file.fileUUID && (
+                <div className={styles.FileBox} style={{ ...cssVariables }}>
+                    <span>{file.originalFileName}</span>
+                    {onDeleteClick && (
+                        <IoMdCloseCircle
+                            className={styles.Delete}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                onDeleteClick();
+                            }}
+                        />
+                    )}
+                </div>
+            )}
+        </>
+    );
+};
+
+export default FileBox;
