521e60d4e8b08eae7a524d73c5db964f112e5dba
mlyykkang <yykkang@mleverage.co.kr>
2025-11-19 11:22:58 +0900

    feat: ImageModalBox 생성

---
+++ ImageModalBox/ImageModalBox.tsx
@@ -0,0 +1,44 @@
+import Modal from '@/shared/headless/Modal/Modal';
+import type { CSSLength } from '@/shared/types';
+import type { CSSVariables } from '@/shared/types/css/CSSVariables';
+import { toCssUnit } from '@/shared/utils';
+import React from 'react';
+import Flex from '../Flex/Flex';
+import styles from './ImageModalBox.module.scss';
+import Portal from '@/shared/headless/Portal/Portal';
+
+type Props = {
+    width?: CSSLength;
+    height?: CSSLength;
+    image: string;
+    showName?: boolean;
+    name?: string;
+};
+const ImageModalBox = ({ width, height, image, showName = false, name }: Props) => {
+    const cssVariables: CSSVariables = {
+        '--width': toCssUnit(width),
+        '--height': toCssUnit(height),
+    };
+    return (
+        <Modal>
+            <Modal.Trigger className={styles.ImageTrigger}>
+                <Flex direction="column">
+                    {image ? (
+                        <img src={`${import.meta.env.VITE_API_URL}api${image}`} style={{ ...cssVariables }} />
+                    ) : (
+                        <div>dd</div>
+                    )}
+                    {showName && name}
+                </Flex>
+            </Modal.Trigger>
+            <Portal>
+                <Modal.Backdrop className={styles.ImageModalBackdrop} />
+                <Modal.Content className={styles.ImageModalContent}>
+                    <img src={`${import.meta.env.VITE_API_URL}api${image}`} />
+                </Modal.Content>
+            </Portal>
+        </Modal>
+    );
+};
+
+export default ImageModalBox;
