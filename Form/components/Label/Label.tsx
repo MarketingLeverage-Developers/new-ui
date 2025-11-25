f3a126447989c5b7f6b4fe8546dff1a7b08215fd
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-11-26 07:25:55 +0900

    feat: Label 수정 - type 속성 추가

--- Form/components/Label/Label.tsx
+++ Form/components/Label/Label.tsx
@@ -3,22 +3,29 @@ import styles from './Label.module.scss';
 import type { CSSVariables } from '@/shared/types/css/CSSVariables';
 import { toCssUnit } from '@/shared/utils';
 import type { CSSLength } from '@/shared/types';
+import classNames from 'classnames';
 
 type LabelProps = {
+    type?: 'vertical' | 'horizontal';
     text: string;
     subText?: string;
     marginBottom?: CSSLength;
     gap?: CSSLength;
 } & React.HTMLAttributes<HTMLDivElement>;
 
-export const Label = ({ text, subText, marginBottom = 0, gap = 8, ...props }: LabelProps) => {
+export const Label = ({ type = 'vertical', text, subText, marginBottom = 0, gap = 8, ...props }: LabelProps) => {
     const cssVariables: CSSVariables = {
         '--margin-bottom': toCssUnit(marginBottom),
         '--gap': toCssUnit(gap),
     };
 
+    const labelClassName = classNames(styles.Label, {
+        [styles.Vertical]: type === 'vertical',
+        [styles.Horizontal]: type === 'horizontal',
+    });
+
     return (
-        <div {...props} className={styles.Label} style={{ ...cssVariables }}>
+        <div {...props} className={labelClassName} style={{ ...cssVariables }}>
             <div className={styles.TextWrapper}>
                 <span className={styles.Text}>{text}</span>
                 <span className={styles.SubText}>{subText}</span>
