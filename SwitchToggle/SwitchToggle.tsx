1b9faec89d8d1df1f34fa487d74cdd21694247a3
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-11-26 07:26:08 +0900

    feat: SwitchToggle 수정 - label 속성 추가

--- SwitchToggle/SwitchToggle.tsx
+++ SwitchToggle/SwitchToggle.tsx
@@ -2,15 +2,25 @@ import Toggle from '@/shared/headless/Toggle/Toggle';
 import React from 'react';
 
 import Trigger from './components/Trigger';
+import Flex from '../Flex/Flex';
+import Text from '../Text/Text';
+import { getThemeColor } from '@/shared/utils/css/getThemeColor';
 
 type SwitchToggleProps = Omit<React.ComponentProps<typeof Toggle>, 'children'> & {
+    label?: React.ReactNode;
     onTriggerClick?: (value: boolean) => void;
 };
 
-const SwitchToggle = ({ defaultValue, onChange, value, onTriggerClick }: SwitchToggleProps) => (
-        <Toggle defaultValue={defaultValue} onChange={onChange} value={value}>
+const SwitchToggle = ({ label, defaultValue, onChange, value, onTriggerClick }: SwitchToggleProps) => (
+    <Toggle defaultValue={defaultValue} onChange={onChange} value={value}>
+        <Flex align="center" gap={12}>
+            <Text fontSize={13} color={getThemeColor('Gray2')}>
+                {label}
+            </Text>
+
             <Trigger onTriggerClick={onTriggerClick} />
-        </Toggle>
-    );
+        </Flex>
+    </Toggle>
+);
 
 export default SwitchToggle;
