ed24eca391f857dd947114757e37dd8b909fee43
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-11-28 13:45:39 +0900

    fix: Table.View 수정 - 테이블 스타일 미적용 이슈 해결

--- StripedTableX/components/View/View.tsx
+++ StripedTableX/components/View/View.tsx
@@ -1,3 +1,6 @@
 import Table from '@/shared/headless/TableX/Table';
+import styles from './View.module.scss';
 
-export const View = (props: React.ComponentProps<typeof Table.View>) => <Table.View {...props} />;
+export const View = (props: React.ComponentProps<typeof Table.View>) => (
+    <Table.View {...props} className={styles.View} />
+);
