f5efb37c160bef90022ed15e977a7e372f114837
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-12-01 09:50:29 +0900

    feat: EmptyFallback 생성

---
+++ EmptyFallback/EmptyFallback.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Flex from '../Flex/Flex';
+import Text from '../Text/Text';
+import { Image } from '../Image/Image';
+import NoDataIcon from '@/shared/assets/images/no-data.png';
+import { getThemeColor } from '@/shared/utils/css/getThemeColor';
+
+const EmptyFallback = () => (
+    <Flex direction="column" align="center" gap={7}>
+        <Image src={NoDataIcon} width={75} alt="데이터 없음 아이콘" />
+        <Text fontSize={15} color={getThemeColor('Gray2')}>
+            {'데이터가 없습니다.'}
+        </Text>
+    </Flex>
+);
+
+export default EmptyFallback;
