20ca56df346304cf60fff752c075be991b7147be
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-12-09 16:30:36 +0900

    feat: EmptyFallback, Item 수정 - 다크모드 값 수정

--- EmptyFallback/EmptyFallback.tsx
+++ EmptyFallback/EmptyFallback.tsx
@@ -8,7 +8,7 @@ import { getThemeColor } from '@/shared/utils/css/getThemeColor';
 const EmptyFallback = () => (
     <Flex direction="column" align="center" gap={7}>
         <Image src={NoDataIcon} width={75} alt="데이터 없음 아이콘" />
-        <Text fontSize={15} color={getThemeColor('Gray2')}>
+        <Text fontSize={15} textColor={getThemeColor('Gray2')}>
             {'데이터가 없습니다.'}
         </Text>
     </Flex>
