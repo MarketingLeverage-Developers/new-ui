114441df23c2b0eec3754a7e19d03c60f2c32b5c
cheolwooklee <lcw5745187@mleverage.co.kr>
2026-01-06 10:05:02 +0900

    feat : BasicDropDown

---
+++ BasicDropDown/components/Trigger/Trigger.tsx
@@ -0,0 +1,31 @@
+import Dropdown from '@/shared/headless/Dropdown/Dropdown';
+import React from 'react';
+import styles from './Trigger.module.scss';
+import { MdChevronLeft, MdChevronRight, MdOutlineKeyboardArrowDown } from 'react-icons/md';
+
+type TriggerProps = {
+    children: React.ReactNode;
+    onPrev: () => void;
+    onNext: () => void;
+    disabledPrev?: boolean;
+    disabledNext?: boolean;
+} & React.ComponentProps<typeof Dropdown.Trigger>;
+
+export const Trigger = ({ children, onPrev, onNext, disabledPrev, disabledNext, ...props }: TriggerProps) => (
+    <div className={styles.Wrapper}>
+        <button type="button" className={styles.NavPrev} onClick={onPrev} disabled={disabledPrev} aria-label="Previous">
+            <MdChevronLeft />
+        </button>
+
+        <Dropdown.Trigger {...props}>
+            <div className={styles.Trigger}>
+                {children}
+                <MdOutlineKeyboardArrowDown className={styles.Arrow} />
+            </div>
+        </Dropdown.Trigger>
+
+        <button type="button" className={styles.NavNext} onClick={onNext} disabled={disabledNext} aria-label="Next">
+            <MdChevronRight />
+        </button>
+    </div>
+);
