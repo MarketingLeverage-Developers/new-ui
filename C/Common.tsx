94ae8885685defdf250163685972a182b04905e0
mlevearge-dhkim <dhkim@mleverage.co.kr>
2026-01-23 14:06:38 +0900

    feat: Common 수정 - Box, Image, Text 복제

--- C/Common.tsx
+++ C/Common.tsx
@@ -1,5 +1,11 @@
 import Flex from './components/Flex/Flex';
+import { Image } from './components/Image/Image';
+import Box from './components/Box/Box';
+import Text from './components/Text/Text';
 
 export const Common = () => {};
 
 Common.Flex = Flex;
+Common.Image = Image;
+Common.Box = Box;
+Common.Text = Text;
---
+++ C/components/Box/Box.tsx
@@ -0,0 +1,57 @@
+import React, { type HTMLAttributes } from 'react';
+
+import BaseBox, { type BaseBoxExtraProps } from './components/BaseBox/BaseBox';
+import FlexBox, { type FlexBoxExtraProps } from './components/FlexBox/FlexBox';
+
+/** CSS 크기 단위 타입 */
+export type CSSLength = number | string;
+
+export type PaddingSize =
+    | number
+    | {
+          x?: number;
+          y?: number;
+          l?: number;
+          r?: number;
+          t?: number;
+          b?: number;
+      };
+
+export type BoxVariant = 'base' | 'flex';
+
+export type BoxCommonProps = HTMLAttributes<HTMLDivElement> & {
+    children: React.ReactNode;
+} & {
+    direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
+    align?: 'stretch' | 'center' | 'start' | 'end' | 'baseline';
+    justify?: 'start' | 'end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
+    gap?: string | number;
+    wrap?: 'nowrap' | 'wrap' | 'wrap-reverse';
+
+    width?: string | number;
+    maxWidth?: CSSLength;
+    height?: string | number;
+
+    padding?: PaddingSize | number;
+    margin?: PaddingSize | number;
+
+    flex?: CSSLength;
+    minWidth?: CSSLength;
+    minHeight?: CSSLength;
+};
+
+export type BoxProps =
+    | ({ variant?: 'flex' } & BoxCommonProps & FlexBoxExtraProps)
+    | ({ variant?: 'base' } & BoxCommonProps & BaseBoxExtraProps);
+
+const Box: React.FC<BoxProps> = (props) => {
+    const { variant = 'flex', ...rest } = props;
+
+    if (variant === 'base') {
+        return <BaseBox {...(rest as BoxCommonProps & BaseBoxExtraProps)} />;
+    }
+
+    return <FlexBox {...(rest as BoxCommonProps & FlexBoxExtraProps)} />;
+};
+
+export default Box;
