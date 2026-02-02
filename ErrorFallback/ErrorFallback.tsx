ac9b52d0bb3b7a7429550a152580b551a0691880
mlevearge-dhkim <dhkim@mleverage.co.kr>
2026-02-02 10:36:19 +0900

    feat: ErrorFallback 수정 - 에러 메시지 받을수 있도록 수정

--- ErrorFallback/ErrorFallback.tsx
+++ ErrorFallback/ErrorFallback.tsx
@@ -8,27 +8,32 @@ import ErrorImage from '@/shared/assets/images/error.svg';
 
 interface ErrorFallbackProps {
     onRetry?: () => void;
+    message?: string;
 }
 
-const ErrorFallback: React.FC<ErrorFallbackProps> = ({ onRetry }) => (
-    <Flex direction="column" gap={16} align="center">
-        <img src={ErrorImage} />
-        <Text fontSize={17} fontWeight={500} textColor={getThemeColor('Gray1')}>
-            일시적인 오류입니다
-        </Text>
-        <Text fontSize={15} fontWeight={400} textColor={getThemeColor('Gray2')}>
-            잠시 후에 다시 시도해 주세요
-        </Text>
-        <BaseButton
-            bgColor={getThemeColor('Primary1')}
-            height={44}
-            padding={{ x: 42, y: 12 }}
-            radius={8}
-            onClick={onRetry}
-        >
-            재시도
-        </BaseButton>
-    </Flex>
-);
+const ErrorFallback: React.FC<ErrorFallbackProps> = ({ onRetry, message }) => {
+    const description = message && message.trim().length > 0 ? message : '잠시 후에 다시 시도해 주세요';
+
+    return (
+        <Flex direction="column" gap={16} align="center">
+            <img src={ErrorImage} />
+            <Text fontSize={17} fontWeight={500} textColor={getThemeColor('Gray1')}>
+                오류가 발생했습니다
+            </Text>
+            <Text fontSize={15} fontWeight={400} textColor={getThemeColor('Gray2')}>
+                {description}
+            </Text>
+            <BaseButton
+                bgColor={getThemeColor('Primary1')}
+                height={44}
+                padding={{ x: 42, y: 12 }}
+                radius={8}
+                onClick={onRetry}
+            >
+                재시도
+            </BaseButton>
+        </Flex>
+    );
+};
 
 export default ErrorFallback;
