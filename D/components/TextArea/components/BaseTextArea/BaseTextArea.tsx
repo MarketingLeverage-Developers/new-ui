119e798cfc91f029b2f6fdf41fd2a770673a6f3a
cheolwooklee <lcw5745187@mleverage.co.kr>
2026-02-04 16:52:57 +0900

    fix : BaseTextArea - text 렌더 오류 수정

--- D/components/TextArea/components/BaseTextArea/BaseTextArea.tsx
+++ D/components/TextArea/components/BaseTextArea/BaseTextArea.tsx
@@ -13,7 +13,19 @@ export type BaseTextAreaExtraProps = {
 export type BaseTextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & BaseTextAreaExtraProps;
 
 const BaseTextArea: React.FC<BaseTextAreaProps> = (props) => {
-    const { className, style, height, ...rest } = props;
+    const { className, style, height, value, defaultValue, ...rest } = props;
+
+    // 텍스트 내의 불필요한 제어 문자(수직 탭 등)를 표준 줄바꿈으로 변경
+    const normalizeText = (text: any) => {
+        if (typeof text !== 'string') return text;
+        return (
+            text
+                // eslint-disable-next-line no-control-regex
+                .replace(new RegExp('\\u000B', 'g'), '\n') // Vertical Tab 제거
+                .replace(new RegExp('\\u00A0', 'g'), ' ') // NBSP를 일반 공백으로
+                .normalize('NFC')
+        ); // 유니코드 정규화
+    };
 
     const rootClassName = classNames(styles.BaseTextArea, className);
 
@@ -21,7 +33,15 @@ const BaseTextArea: React.FC<BaseTextAreaProps> = (props) => {
         '--height': toCssUnit(height),
     };
 
-    return <textarea {...rest} className={rootClassName} style={{ ...cssVariables, ...style }} />;
+    return (
+        <textarea
+            {...rest}
+            value={value !== undefined ? normalizeText(value) : undefined}
+            defaultValue={defaultValue !== undefined ? normalizeText(defaultValue) : undefined}
+            className={rootClassName}
+            style={{ ...cssVariables, ...style }}
+        />
+    );
 };
 
 export default BaseTextArea;
