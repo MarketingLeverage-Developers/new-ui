fd95a43901a2f036c312dbecf603236b4685684c
cheolwooklee <lcw5745187@mleverage.co.kr>
2026-02-12 17:25:56 +0900

    fix : BorderRoundedSelect display css 수정

--- BorderRoundedSelect/components/Display/Display.tsx
+++ BorderRoundedSelect/components/Display/Display.tsx
@@ -4,16 +4,37 @@ import React from 'react';
 import styles from './Display.module.scss';
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
 import classNames from 'classnames';
+import type { PaddingSize } from '@/shared/types/css/PaddingSize';
+import { toCssPadding } from '@/shared/utils/css/toCssPadding';
+import type { CSSVariables } from '@/shared/types/css/CSSVariables';
+import type { CSSLength } from '@/shared/types/css/CSSLength';
+import { toCssUnit } from '@/shared/utils/css/toCssUnit';
 
-type DisplayProps = React.ComponentProps<typeof Select.Display> & { fullWidth?: boolean };
+type DisplayProps = React.ComponentProps<typeof Select.Display> & {
+    fullWidth?: boolean;
+    padding?: PaddingSize | number;
+    width?: CSSLength;
+    height?: CSSLength;
+};
 
-export const Display = ({ fullWidth = false, ...props }: DisplayProps) => {
+export const Display = ({
+    fullWidth = false,
+    padding = { x: 16, y: 13 },
+    width,
+    height,
+    ...props
+}: DisplayProps) => {
+    const cssVariables: CSSVariables = {
+        '--padding': toCssPadding(padding),
+        '--width': toCssUnit(width),
+        '--height': toCssUnit(height),
+    };
     const BorderRoundedSelectDisplayclassName = classNames(styles.Display, {
         [styles.FullWidth]: fullWidth,
     });
 
     return (
-        <Dropdown.Trigger className={BorderRoundedSelectDisplayclassName}>
+        <Dropdown.Trigger className={BorderRoundedSelectDisplayclassName} style={cssVariables}>
             <Select.Display {...props} className={styles.Content} />
             <MdOutlineKeyboardArrowDown className={styles.Arrow} />
         </Dropdown.Trigger>
