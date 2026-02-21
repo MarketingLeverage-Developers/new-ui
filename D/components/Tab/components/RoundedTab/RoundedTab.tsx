d77693258a5751d250dc1ef7b8e724d0115e06ba
dhkim <dhkim@mleverage.co.kr>
2026-02-22 08:59:18 +0900

    feat: RoundedTab 수정 - full 속성 추가

--- D/components/Tab/components/RoundedTab/RoundedTab.tsx
+++ D/components/Tab/components/RoundedTab/RoundedTab.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 import styles from './RoundedTab.module.scss';
 import Select from '../../../../../shared/headless/Select/Select';
 import { RoundedTabItem, type RoundedTabItemProps } from './components/Item';
+import classNames from 'classnames';
 
 export type RoundedTabProps = React.ComponentProps<typeof Select> & {
     divProps?: Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'>;
+    full?: boolean;
 };
 
-const RoundedTab: React.FC<RoundedTabProps> & { Item: React.FC<RoundedTabItemProps> } = ({ children, divProps, ...props }) => {
+const RoundedTab: React.FC<RoundedTabProps> & { Item: React.FC<RoundedTabItemProps> } = ({
+    children,
+    divProps,
+    full = false,
+    ...props
+}) => {
     const { style, ...restDiv } = divProps ?? {};
+    const rootClassName = classNames(styles.RoundedTab, { [styles.Full]: full });
 
     return (
         <Select {...props}>
-            <div {...restDiv} className={styles.RoundedTab} style={{ ...style }}>
+            <div {...restDiv} className={rootClassName} style={{ ...style }}>
                 {children}
             </div>
         </Select>
