291ae51fc6d5c046508b03fbaa7734ed92f904d0
dhkim <dhkim@mleverage.co.kr>
2026-02-22 16:54:09 +0900

    feat: ModalTemplate 수정 - onClose 속성 부재시, 닫기 버튼 노출 제한

--- D/components/Template/components/ModalTemplate/components/ModalTemplateHeader/ModalTemplateHeader.tsx
+++ D/components/Template/components/ModalTemplate/components/ModalTemplateHeader/ModalTemplateHeader.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import classNames from 'classnames';
 import styles from './ModalTemplateHeader.module.scss';
 import { MdClose } from 'react-icons/md';
-import { useModal } from '../../../../../../../shared/headless/Modal/Modal';
 
 export type ModalTemplateHeaderProps = {
     className?: string;
@@ -16,21 +15,22 @@ export type ModalTemplateHeaderProps = {
 
 const ModalTemplateHeader: React.FC<ModalTemplateHeaderProps> = (props) => {
     const { className, title, subTitle, onTitleChange, placeholder, onClose, actions } = props;
-    const { closeModal } = useModal();
-    const handleClose = onClose ?? closeModal;
-
     const rootClassName = classNames(styles.ModalTemplateHeader, className);
 
     return (
         <div className={rootClassName}>
             <div className={styles.TopRow}>
                 <div className={styles.SubTitle}>{subTitle}</div>
-                <div className={styles.Right}>
-                    {actions ? <div className={styles.Actions}>{actions}</div> : null}
-                    <button type="button" className={styles.CloseButton} onClick={handleClose} aria-label="close">
-                        <MdClose />
-                    </button>
-                </div>
+                {actions || onClose ? (
+                    <div className={styles.Right}>
+                        {actions ? <div className={styles.Actions}>{actions}</div> : null}
+                        {onClose ? (
+                            <button type="button" className={styles.CloseButton} onClick={onClose} aria-label="close">
+                                <MdClose />
+                            </button>
+                        ) : null}
+                    </div>
+                ) : null}
             </div>
             {onTitleChange ? (
                 <input
