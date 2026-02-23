125faadd00246b5f37b8b2e74364e6f8c1b37ced
cheolwooklee <lcw5745187@mleverage.co.kr>
2026-02-23 16:57:25 +0900

    fix : ButtonTab color props 추가

--- D/components/Tab/components/ButtonTab/components/ButtonTab/ButtonTab.tsx
+++ D/components/Tab/components/ButtonTab/components/ButtonTab/ButtonTab.tsx
@@ -3,10 +3,14 @@ import classNames from 'classnames';
 import styles from './ButtonTab.module.scss';
 
 import Select from '../../../../../../../shared/headless/Select/Select';
+import type { CSSVariables } from '../../../../../../../shared/types/css/CSSVariables';
 import ButtonTabItem, { type ButtonTabItemProps } from './components/ButtonTabItem/ButtonTabItem';
 
 export type ButtonTabExtraProps = {
     className?: string;
+    activeBg?: string;
+    activeBorder?: string;
+    activeTextColor?: string;
 };
 
 export type ButtonTabProps = React.ComponentProps<typeof Select> & ButtonTabExtraProps;
@@ -16,12 +20,17 @@ type ButtonTabCompound = React.FC<ButtonTabProps> & {
 };
 
 const ButtonTabRoot: React.FC<ButtonTabProps> = (props) => {
-    const { className, children, ...selectProps } = props;
+    const { className, activeBg, activeBorder, activeTextColor, children, ...selectProps } = props;
 
     const rootClassName = classNames(styles.ButtonTab, className);
+    const cssVariables: CSSVariables = {
+        '--active-bg': activeBg,
+        '--active-border': activeBorder,
+        '--active-text-color': activeTextColor,
+    };
 
     return (
-        <div className={rootClassName}>
+        <div className={rootClassName} style={cssVariables}>
             <Select {...selectProps}>{children}</Select>
         </div>
     );
