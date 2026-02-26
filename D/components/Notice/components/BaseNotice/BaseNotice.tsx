49ad524e302416a6bdf7760f5a70b2b4bc22cca7
cheolwooklee <lcw5745187@mleverage.co.kr>
2026-02-27 08:55:27 +0900

    fix : notice ui 수정

--- D/components/Notice/components/BaseNotice/BaseNotice.tsx
+++ D/components/Notice/components/BaseNotice/BaseNotice.tsx
@@ -2,6 +2,7 @@ import React, { type HTMLAttributes, type ReactNode } from 'react';
 import classNames from 'classnames';
 import { FiInfo } from 'react-icons/fi';
 import styles from './BaseNotice.module.scss';
+import { AiFillInfoCircle } from 'react-icons/ai';
 
 export type BaseNoticeStatus = 'default' | 'info' | 'error' | 'warning';
 
@@ -28,7 +29,8 @@ const BaseNotice = (props: BaseNoticeProps) => {
         <div className={rootClassName} {...rest}>
             {status === 'info' ? (
                 <span className={styles.Icon}>
-                    <FiInfo />
+                    {/* <FiInfo /> */}
+                    <AiFillInfoCircle />
                 </span>
             ) : null}
             <span className={styles.Text}>{text}</span>
