66ec7c4820ce6d307af9d3b742b82e80d9cf9524
cheolwooklee <lcw5745187@mleverage.co.kr>
2026-02-27 12:02:02 +0900

    fix : tabledSetting Trigger UI 수정

--- BasicTable/components/TableSettingTrigger/TableSettingTrigger.tsx
+++ BasicTable/components/TableSettingTrigger/TableSettingTrigger.tsx
@@ -1,21 +1,25 @@
 import BaseButton from '../../../BaseButton/BaseButton';
 import tableStyles from '../../BasicTable.module.scss';
 import { getThemeColor } from '../../../shared/utils/css/getThemeColor';
-import { FaCog } from 'react-icons/fa';
+// import { FaCog } from 'react-icons/fa';
+import { IoSettingsOutline } from "react-icons/io5";
 
 const TableSettingTrigger = ({ open, onToggle }: { open: boolean; onToggle: () => void }) => (
     <BaseButton
         type="button"
         className={tableStyles.Trigger} // ✅ 이 Trigger도 BasicTable.module.scss로 옮길 수 있음(원하면)
-        padding={{ x: 12, y: 8 }}
-        bgColor={getThemeColor('Gray6')}
+        // padding={{ x: 12, y: 8 }}
+        bgColor={getThemeColor('White1')}
         textColor={getThemeColor('Gray1')}
+        style={{ border: `1px solid ${getThemeColor('Gray5')}` }}
         radius={8}
-        height={34}
+        height={40}
+        width={40}
         fontSize={18}
         onClick={onToggle}
     >
-        <FaCog />
+        {/* <FaCog /> */}
+        <IoSettingsOutline />
     </BaseButton>
 );
 
