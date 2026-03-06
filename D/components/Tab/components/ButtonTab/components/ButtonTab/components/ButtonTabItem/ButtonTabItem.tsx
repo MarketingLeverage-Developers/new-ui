941caa7db21f7671585776f3863b6a6c02f40ab0
cheolwooklee <lcw5745187@mleverage.co.kr>
2026-03-06 15:40:29 +0900

    fix : tab item 의 text color props 추가

--- D/components/Tab/components/ButtonTab/components/ButtonTab/components/ButtonTabItem/ButtonTabItem.tsx
+++ D/components/Tab/components/ButtonTab/components/ButtonTab/components/ButtonTabItem/ButtonTabItem.tsx
@@ -3,17 +3,25 @@ import classNames from 'classnames';
 import styles from './ButtonTabItem.module.scss';
 
 import Select, { useSelect } from '../../../../../../../../../shared/headless/Select/Select';
+import type { CSSVariables } from '../../../../../../../../../shared/types/css/CSSVariables';
+import type { HexColor } from '../../../../../../../../../shared/types/css/HexColor';
+import type { ThemeColorVar } from '../../../../../../../../../shared/types/css/ThemeColorTokens';
 
 export type ButtonTabItemProps = Omit<React.ComponentProps<typeof Select.Item>, 'value'> & {
     value: string;
     className?: string;
+    textColor?: HexColor | ThemeColorVar;
 };
 
 const ButtonTabItem: React.FC<ButtonTabItemProps> = (props) => {
-    const { className, value, children, ...rest } = props;
+    const { className, value, children, style, textColor, ...rest } = props;
 
     const { isActive } = useSelect();
     const active = isActive(value);
+    const cssVariables: CSSVariables = {
+        '--text-color': textColor,
+        '--active-text-color': textColor,
+    };
 
     const itemClassName = classNames(
         styles.Item,
@@ -24,7 +32,7 @@ const ButtonTabItem: React.FC<ButtonTabItemProps> = (props) => {
     );
 
     return (
-        <Select.Item value={value} {...rest} className={itemClassName}>
+        <Select.Item value={value} {...rest} className={itemClassName} style={{ ...cssVariables, ...style }}>
             {children}
         </Select.Item>
     );
--- D/components/Tab/components/RoundedTab/components/Item.tsx
+++ D/components/Tab/components/RoundedTab/components/Item.tsx
@@ -3,18 +3,27 @@ import { useSelect } from '../../../../../../shared/headless/Select/Select';
 import classNames from 'classnames';
 import styles from '../RoundedTab.module.scss';
 import React from 'react';
+import type { CSSVariables } from '../../../../../../shared/types/css/CSSVariables';
+import type { HexColor } from '../../../../../../shared/types/css/HexColor';
+import type { ThemeColorVar } from '../../../../../../shared/types/css/ThemeColorTokens';
 
-export type RoundedTabItemProps = React.ComponentProps<typeof Select.Item>;
+export type RoundedTabItemProps = React.ComponentProps<typeof Select.Item> & {
+    textColor?: HexColor | ThemeColorVar;
+};
 
-export const RoundedTabItem: React.FC<RoundedTabItemProps> = ({ style, ...props }) => {
+export const RoundedTabItem: React.FC<RoundedTabItemProps> = ({ style, textColor, ...props }) => {
     const { isActive } = useSelect();
+    const cssVariables: CSSVariables = {
+        '--text-color': textColor,
+        '--active-text-color': textColor,
+    };
 
     const itemclassName = classNames(styles.Item, {
         [styles.Active]: isActive(props.value),
     });
 
     return (
-        <Select.Item {...props} className={itemclassName} style={{ ...style }}>
+        <Select.Item {...props} className={itemclassName} style={{ ...cssVariables, ...style }}>
             {props.children}
         </Select.Item>
     );
