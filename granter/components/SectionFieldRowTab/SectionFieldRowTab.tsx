fa3816d7e2c0b7c14f0419082632736577aa9e25
mlevearge-dhkim <dhkim@mleverage.co.kr>
2026-03-27 17:24:26 +0900

    feat: SectionFieldRowTab 수정 - 셀렉트 탭으로 수정

--- granter/components/SectionFieldRowTab/SectionFieldRowTab.tsx
+++ granter/components/SectionFieldRowTab/SectionFieldRowTab.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import SectionFieldTab, {
+    type SectionFieldTabDropdownItemProps,
     type SectionFieldTabItemProps,
     type SectionFieldTabProps,
 } from '../SectionFieldTab/SectionFieldTab';
 
 export type SectionFieldRowTabProps<T extends string = string> = SectionFieldTabProps<T>;
 export type SectionFieldRowTabItemProps<T extends string = string> = SectionFieldTabItemProps<T>;
+export type SectionFieldRowTabDropdownItemProps<T extends string = string> = SectionFieldTabDropdownItemProps<T>;
 
 type SectionFieldRowTabComponent = (<T extends string = string>(props: SectionFieldRowTabProps<T>) => React.ReactElement) & {
     Item: typeof SectionFieldTab.Item;
+    DropdownItem: typeof SectionFieldTab.DropdownItem;
 };
 
 const SectionFieldRowTab = ((props) => <SectionFieldTab {...props} />) as SectionFieldRowTabComponent;
 
 SectionFieldRowTab.Item = SectionFieldTab.Item;
+SectionFieldRowTab.DropdownItem = SectionFieldTab.DropdownItem;
 
 export default SectionFieldRowTab;
--- granter/components/SectionFieldTab/SectionFieldTab.tsx
+++ granter/components/SectionFieldTab/SectionFieldTab.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import classNames from 'classnames';
+import { HiOutlineChevronDown } from 'react-icons/hi2';
 import Select, { useSelect } from '../../../shared/headless/Select/Select';
+import Dropdown, { useDropdown } from '../../../shared/headless/Dropdown/Dropdown';
 import styles from './SectionFieldTab.module.scss';
 
 const noop = () => undefined;
@@ -25,6 +27,23 @@ export type SectionFieldTabItemProps<T extends string = string> = {
     children: React.ReactNode;
     disabled?: boolean;
     className?: string;
+};
+
+export type SectionFieldTabDropdownItemOption<T extends string = string> = {
+    value: T;
+    label: React.ReactNode;
+    disabled?: boolean;
+};
+
+export type SectionFieldTabDropdownItemProps<T extends string = string> = {
+    children: React.ReactNode;
+    options: SectionFieldTabDropdownItemOption<T>[];
+    disabled?: boolean;
+    className?: string;
+    menuClassName?: string;
+    optionClassName?: string;
+    placement?: React.ComponentProps<typeof Dropdown.Content>['placement'];
+    dropdownIcon?: React.ReactNode;
 };
 
 const SectionFieldTabItem = <T extends string>({
@@ -54,8 +73,107 @@ const SectionFieldTabItem = <T extends string>({
     );
 };
 
+const DropdownTriggerContent = ({
+    children,
+    dropdownIcon,
+}: {
+    children: React.ReactNode;
+    dropdownIcon?: React.ReactNode;
+}) => {
+    const { isOpen } = useDropdown();
+
+    return (
+        <>
+            <span className={styles.DropdownLabel}>{children}</span>
+            <span className={styles.DropdownChevron} data-open={isOpen ? 'true' : 'false'}>
+                {dropdownIcon ?? <HiOutlineChevronDown size={16} />}
+            </span>
+        </>
+    );
+};
+
+const SectionFieldTabDropdownItem = <T extends string>({
+    children,
+    options,
+    disabled = false,
+    className,
+    menuClassName,
+    optionClassName,
+    placement = 'bottom-end',
+    dropdownIcon,
+}: SectionFieldTabDropdownItemProps<T>) => {
+    const { selectValue, changeSelectValue } = useSelect();
+    const isActive = options.some((option) => option.value === selectValue);
+
+    return (
+        <Dropdown>
+            <Dropdown.Trigger
+                role="tab"
+                tabIndex={disabled ? -1 : 0}
+                aria-selected={isActive}
+                className={classNames(styles.Item, styles.DropdownItem, className)}
+                data-active={isActive ? 'true' : 'false'}
+                disabled={disabled}
+            >
+                <DropdownTriggerContent dropdownIcon={dropdownIcon}>{children}</DropdownTriggerContent>
+            </Dropdown.Trigger>
+            <DropdownMenu
+                options={options}
+                selectValue={selectValue as T | undefined}
+                onSelect={changeSelectValue}
+                disabled={disabled}
+                menuClassName={menuClassName}
+                optionClassName={optionClassName}
+                placement={placement}
+            />
+        </Dropdown>
+    );
+};
+
+const DropdownMenu = <T extends string>({
+    options,
+    selectValue,
+    onSelect,
+    disabled,
+    menuClassName,
+    optionClassName,
+    placement,
+}: {
+    options: SectionFieldTabDropdownItemOption<T>[];
+    selectValue?: T;
+    onSelect: (value: T) => void;
+    disabled: boolean;
+    menuClassName?: string;
+    optionClassName?: string;
+    placement: React.ComponentProps<typeof Dropdown.Content>['placement'];
+}) => {
+    const { close } = useDropdown();
+
+    return (
+        <Dropdown.Content className={classNames(styles.DropdownMenu, menuClassName)} placement={placement}>
+            {options.map((option) => (
+                <button
+                    key={option.value}
+                    type="button"
+                    className={classNames(styles.DropdownOption, optionClassName)}
+                    data-active={option.value === selectValue ? 'true' : 'false'}
+                    disabled={disabled || option.disabled}
+                    onClick={() => {
+                        if (disabled || option.disabled) return;
+                        onSelect(option.value);
+                        close();
+                    }}
+                >
+                    {option.label}
+                </button>
+            ))}
+        </Dropdown.Content>
+    );
+};
+
 type SectionFieldTabComponent = (<T extends string = string>(props: SectionFieldTabProps<T>) => React.ReactElement) & {
     Item: typeof SectionFieldTabItem;
+    DropdownItem: typeof SectionFieldTabDropdownItem;
 };
 
 const toCssLength = (value?: number | string) => {
@@ -68,8 +186,8 @@ const SectionFieldTab = (<T extends string = string>({
     value,
     defaultValue,
     onChange = noop,
-    full = true,
-    wrap = false,
+    full: _full = true,
+    wrap: _wrap = false,
     minItemWidth,
     className,
 }: SectionFieldTabProps<T>) => {
@@ -97,5 +215,6 @@ const SectionFieldTab = (<T extends string = string>({
 }) as SectionFieldTabComponent;
 
 SectionFieldTab.Item = SectionFieldTabItem;
+SectionFieldTab.DropdownItem = SectionFieldTabDropdownItem;
 
 export default SectionFieldTab;
